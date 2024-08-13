d6ac7c82ac5b26d1cf3c82e4abaac627d837fea3
Elias Tagle <eliastzv@hotmail.com>
2024-08-13 15:36:05 -0500

    se modifico el properties para configurar la db, se agrego la entidad users, el enum de roles, la configuracion para crear la tabla users y se agrego archivo del schema

---
+++ security-service/src/main/java/org/zerocool/securityservice/config/DBConfig.java
@@ -0,0 +1,31 @@
+package org.zerocool.securityservice.config;
+
+import io.r2dbc.spi.ConnectionFactories;
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
+import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+
+public class DBConfig extends AbstractR2dbcConfiguration {
+
+    @Value("${spring.r2dbc.url}")
+    private String dbURL;
+    @Override
+    public ConnectionFactory connectionFactory() {
+        return ConnectionFactories.get(dbURL);
+    }
+
+    @Bean
+    public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory){
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        CompositeDatabasePopulator populator = new CompositeDatabasePopulator();
+        populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
+        initializer.setDatabasePopulator(populator);
+        return initializer;
+    }
+}
---
+++ security-service/src/main/java/org/zerocool/securityservice/security/entity/Users.java
@@ -0,0 +1,58 @@
+package org.zerocool.securityservice.security.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Data
+@Table
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Users implements UserDetails {
+    @Id
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private String roles;
+    private Boolean enabled;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Stream.of(roles.split(", "))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
