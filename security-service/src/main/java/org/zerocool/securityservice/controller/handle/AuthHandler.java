b2c28ed64870c194bb2aa3e66ed6c30a743dd404
Elias Tagle <eliastzv@hotmail.com>
2024-08-13 17:44:44 -0500

    Se agrego ObjectValidator para validar los objetos de las peticiones el cual se integro en el AuthHandler

--- security-service/src/main/java/org/zerocool/securityservice/controller/handle/AuthHandler.java
+++ security-service/src/main/java/org/zerocool/securityservice/controller/handle/AuthHandler.java
@@ -8,20 +8,23 @@ import org.zerocool.securityservice.adapter.dto.LoginDTO;
 import org.zerocool.securityservice.adapter.dto.UserDTO;
 import org.zerocool.securityservice.adapter.port.in.UserPort;
 import org.zerocool.securityservice.adapter.port.out.UserRepositoryPort;
+import org.zerocool.securityservice.common.validation.ObjectValidator;
 import org.zerocool.securityservice.domain.dto.TokenDTO;
 import reactor.core.publisher.Mono;
 
 @Component
 public class AuthHandler implements UserPort {
     private final UserRepositoryPort userRepositoryPort;
+    private final ObjectValidator objectValidator;
 
-    public AuthHandler(UserRepositoryPort userRepositoryPort) {
+    public AuthHandler(UserRepositoryPort userRepositoryPort, ObjectValidator objectValidator) {
         this.userRepositoryPort = userRepositoryPort;
+        this.objectValidator = objectValidator;
     }
 
     @Override
     public Mono<ServerResponse> login(ServerRequest request) {
-        Mono<LoginDTO> login = request.bodyToMono(LoginDTO.class);
+        Mono<LoginDTO> login = request.bodyToMono(LoginDTO.class).doOnNext(objectValidator::validate);
         return login
                 .flatMap(dto -> ServerResponse.ok()
                         .contentType(MediaType.APPLICATION_JSON)
@@ -31,7 +34,7 @@ public class AuthHandler implements UserPort {
 
     @Override
     public Mono<ServerResponse> registry(ServerRequest request) {
-        Mono<UserDTO> user = request.bodyToMono(UserDTO.class);
+        Mono<UserDTO> user = request.bodyToMono(UserDTO.class).doOnNext(objectValidator::validate);
         return user
                 .flatMap(dto -> ServerResponse.ok()
                         .contentType(MediaType.APPLICATION_JSON)
