eb63c939091dbed8d5c225518e2129db86498820
Elias Tagle <eliastzv@hotmail.com>
2024-08-13 18:35:34 -0500

    eliminacion de dependencias de test que no dejaban crear el jar del proyecto

--- security-service/src/test/java/org/zerocool/securityservice/SecurityServiceApplicationTests.java
+++ security-service/src/test/java/org/zerocool/securityservice/SecurityServiceApplicationTests.java
@@ -1,13 +1,4 @@
 package org.zerocool.securityservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SecurityServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
-}
