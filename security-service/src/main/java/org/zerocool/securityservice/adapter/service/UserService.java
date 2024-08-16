37650cdf2f54949f5960e1ef67e56548b1f50342
Elias Tagle <eliastzv@hotmail.com>
2024-08-15 22:53:33 -0500

    Se agrego en el service el metodo refresh token y se modifico el tokenDTO para que devuelva el atributo refresh_token

--- security-service/src/main/java/org/zerocool/securityservice/adapter/service/UserService.java
+++ security-service/src/main/java/org/zerocool/securityservice/adapter/service/UserService.java
@@ -1,19 +1,26 @@
 package org.zerocool.securityservice.adapter.service;
 
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.zerocool.securityservice.adapter.dto.LoginDTO;
+import org.zerocool.securityservice.adapter.dto.UserDTO;
 import org.zerocool.securityservice.adapter.port.out.UserRepositoryPort;
 import org.zerocool.securityservice.adapter.repository.UserRepository;
 import org.zerocool.securityservice.common.exception.CustomException;
 import org.zerocool.securityservice.domain.dto.TokenDTO;
-import org.zerocool.securityservice.adapter.dto.UserDTO;
 import org.zerocool.securityservice.domain.emuns.Roles;
 import org.zerocool.securityservice.domain.entity.Users;
 import org.zerocool.securityservice.domain.jwt.JwtProvider;
 import reactor.core.publisher.Mono;
+
+import java.util.Date;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -48,7 +55,28 @@ public class UserService implements UserRepositoryPort {
         return userRepository
                 .findByUsernameOrEmail(loginDTO.getUsername(), loginDTO.getUsername())
                 .filter(users -> passwordEncoder.matches(loginDTO.getPassword(),users.getPassword()))
-                .map(user -> new TokenDTO(jwtProvider.generateToken(user)))
+                .map(jwtProvider::createToken)
                 .switchIfEmpty(Mono.error(new CustomException("User not found", HttpStatus.BAD_REQUEST)));
+    }
+
+    @Override
+    public Mono<TokenDTO> refreshToken(TokenDTO tokenDTO) {
+
+        Claims claimsRefreshToken = jwtProvider.getClaims(tokenDTO.getTokenRefresh());
+        Claims claimsToken = jwtProvider.getClaims(tokenDTO.getToken());
+
+        if(claimsRefreshToken.getExpiration().before(new Date())){
+            return Mono.error(new CustomException("Refresh Token expired", HttpStatus.UNAUTHORIZED));
+        }
+
+        List<SimpleGrantedAuthority> roles = jwtProvider.getRoles(claimsToken);
+
+        UserDetails userDetails = User.builder()
+                .username(claimsToken.getSubject())
+                .authorities(roles)
+                .password("")
+                .build();
+
+        return Mono.just(jwtProvider.createToken(userDetails));
     }
 }
--- security-service/src/main/java/org/zerocool/securityservice/domain/dto/TokenDTO.java
+++ security-service/src/main/java/org/zerocool/securityservice/domain/dto/TokenDTO.java
@@ -1,12 +1,17 @@
 package org.zerocool.securityservice.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class TokenDTO {
     private String token;
+    @JsonProperty("token_refresh")
+    private String tokenRefresh;
 }
