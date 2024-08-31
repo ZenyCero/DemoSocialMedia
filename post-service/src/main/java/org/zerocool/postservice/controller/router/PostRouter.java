10842ead6c562d14cfba1aa194a72d507b22be1d
Elias Tagle <eliastzv@hotmail.com>
2024-08-31 16:19:36 -0500

    Se agrego el handler y router de post ademas del contrato in PostPort

---
+++ post-service/src/main/java/org/zerocool/postservice/controller/router/PostRouter.java
@@ -0,0 +1,20 @@
+package org.zerocool.postservice.controller.router;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.zerocool.postservice.controller.handler.PostHandler;
+
+@Configuration
+public class PostRouter {
+    private static final String PATH = "post/";
+    @Bean
+    RouterFunction<ServerResponse> routerPost(PostHandler handler){
+        return RouterFunctions.route()
+                .POST(PATH, handler::save)
+                .GET(PATH + "id", handler::get)
+                .build();
+    }
+}
