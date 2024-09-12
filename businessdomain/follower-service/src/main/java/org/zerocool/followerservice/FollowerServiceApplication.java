f3607ab06c2857947a936760d4c342f80fba2028
Elias T <eliastzv@hotmail.com>
2024-09-11 21:01:09 -0500

    creacion del servicio follower

---
+++ businessdomain/follower-service/src/main/java/org/zerocool/followerservice/FollowerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.zerocool.followerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FollowerServiceApplication.class, args);
+    }
+
+}
---
+++ businessdomain/follower-service/src/test/java/org/zerocool/followerservice/FollowerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerocool.followerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
