7adbc1326bf227880368e6f9907c63abe674aa6f
Elias T <eliastzv@hotmail.com>
2024-09-11 20:59:14 -0500

    eliminacion de contador para auto incrementar el id en post

--- businessdomain/post-service/src/main/java/org/zerocool/postservice/adapter/repository/CounterPostRepository.java
+++
@@ -1,9 +0,0 @@
-package org.zerocool.postservice.adapter.repository;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-import org.zerocool.postservice.adapter.entity.CounterPost;
-
-@Repository
-public interface CounterPostRepository extends ReactiveMongoRepository<CounterPost, String> {
-}
